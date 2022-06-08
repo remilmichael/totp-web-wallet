8db8c83fdafec4ce772401e02f4f18756f4d3518
Remil Michael <mail@remil.me>
2022-06-08 00:46:21 -0400

    add sample register page

---
+++ src/containers/register/Form.js
@@ -0,0 +1,44 @@
+import React from "react";
+
+function Form(props) {
+  // destructure
+  const {
+    emailInputHandler,
+    passwordInputHandler,
+    confirmPassHandler,
+    formSubmitHandler,
+  } = props;
+
+  function submitForm(e) {
+    e.preventDefault();
+    formSubmitHandler();
+  }
+
+  return (
+    <>
+      <form>
+        <label htmlFor="email">Email:</label>
+        <input
+          type="email"
+          id="email"
+          onChange={(e) => emailInputHandler(e.target.value)}
+        />
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          id="password"
+          onChange={(e) => passwordInputHandler(e.target.value)}
+        />
+        <label htmlFor="confirmPass">Confirm Password:</label>
+        <input
+          type="password"
+          id="confirmPass"
+          onChange={(e) => confirmPassHandler(e.target.value)}
+        />
+        <input type="submit" onClick={(e) => submitForm(e)} />
+      </form>
+    </>
+  );
+}
+
+export default Form;
---
+++ src/containers/register/Register.js
@@ -0,0 +1,70 @@
+import Form from "./Form";
+import React, { useState } from "react";
+import CryptoJs from "crypto-js";
+import axios from "axios";
+import { BASE_URL, rfc5054 } from "../../env";
+
+function Register() {
+
+  const SRP6JavascriptClientSession  = require('thinbus-srp/browser')(rfc5054.N_base10, rfc5054.g_base10, rfc5054.k_base16);
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPass, setConfirmPass] = useState("");
+
+  const formSubmitHandler = async () => {
+    if (password !== confirmPass) {
+        // error
+    } else {
+
+        const response = await axios.get(`${BASE_URL}/check-username?email=${email}`)
+        if (!response) {
+          //failed
+        } else if (response.data.userExist) {
+          console.log('email already exists')
+        } else {
+          const credentialsObj = generateCredentials();
+          console.log(credentialsObj)
+            axios.post(`${BASE_URL}/register`, credentialsObj)
+              .then(response => {
+                console.log(response);
+              })
+        }
+    }
+  }
+
+  const generateCredentials = () => {
+    const salt = CryptoJs.lib.WordArray.random(128 / 8);
+    const key256Bits = CryptoJs.PBKDF2(email + password + new Date().getTime(), salt, {
+        keySize: 256 / 32,
+    });
+    const encryptedKey = CryptoJs.AES.encrypt(key256Bits.toString(), password);
+    const hmac = CryptoJs.HmacSHA256(encryptedKey, email + password); // add salt later
+    console.log(hmac)
+    const finalKey = hmac.toString() + encryptedKey.toString();
+
+
+    const client = new SRP6JavascriptClientSession();
+    const saltForVerifier = client.generateRandomSalt(); 
+    const verifier = client.generateVerifier(saltForVerifier, email, password);
+
+    return {
+      email,
+      salt: saltForVerifier,
+      verifier,
+      encKey: finalKey,
+    };
+  }
+
+
+  return (
+    <Form
+      emailInputHandler={(input) => setEmail(input)}
+      passwordInputHandler={(input) => setPassword(input)}
+      confirmPassHandler={(input) => setConfirmPass(input)}
+      formSubmitHandler={formSubmitHandler}
+    />
+  );
+}
+
+export default Register;
