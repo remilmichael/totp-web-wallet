bf9e9cc218ba84012e4cfe4a30436b2eccc1b11c
Remil Michael <mail@remil.me>
2022-07-29 19:00:20 -0400

    add submit contact form functionality

--- src/ContactUs.js
+++ src/ContactUs.js
@@ -3,22 +3,74 @@ import BreadCrumb from "./components/BreadCrumb/BreadCrumb";
 import ContactContent from "./components/ContactContent/ContactContent";
 import HomeFooter from "./components/HomeFooter/HomeFooter";
 import Navbar from "./components/Navbar/Navbar";
+import { instance } from "./constants";
 
 function ContactUs() {
-
   React.useEffect(() => {
     document.body.style.backgroundColor = "#FFF";
 
     return () => {
       document.body.style.backgroundColor = "#EAF3FA";
+      setOutput(null);
     };
   }, []);
+
+  const [name, setName] = React.useState("");
+  const [email, setEmail] = React.useState("");
+  const [subject, setSubject] = React.useState("");
+  const [message, setMessage] = React.useState("");
+  const [output, setOutput] = React.useState("");
+  const [spinner, setSpinner] = React.useState(false);
+
+  const resetStates = () => {
+    setName("")
+    setEmail("");
+    setSubject("");
+    setMessage("");
+  }
+
+  const formSubmitHandler = () => {
+    setSpinner(true);
+    if (name && email && subject && message) {
+      const body = {
+        name: name, 
+        email: email,
+        subject: subject,
+        message: message
+      }
+      instance.post('/ticket/add', body)
+        .then(resp => {
+          setOutput("Message has been submitted. We will respond to you soon.");
+          setSpinner(false);
+          resetStates();
+        })
+        .catch(err => {
+          resetStates();
+          if (err.response && err.response.data &&  err.response.data.message) {
+            setOutput(err.response.data.message);
+          } else {
+            setOutput("Something went wrong");
+          }
+          setSpinner(false);
+        })
+    } else {
+      setOutput("All fields are required!!!")
+    }
+  };
 
   return (
     <>
       <Navbar />
       <BreadCrumb name="Contact Us" desc="Contact Us" />
-      <ContactContent />
+      <ContactContent
+        nameHandler={setName}
+        emailHandler={setEmail}
+        subjectHandler={setSubject}
+        messageHandler={setMessage}
+        formSubmitHandler={formSubmitHandler}
+        output={output}
+        spinnerState={spinner}
+      />
       <HomeFooter />
     </>
   );
--- src/components/ContactContent/ContactContent.js
+++ src/components/ContactContent/ContactContent.js
@@ -1,29 +1,74 @@
-import ContactImage from '../../assets/contact.svg';
+import ContactImage from "../../assets/contact.svg";
+import SmallSpinner from '../SmallSpinner/SmallSpinner';
 
-function ContactContent() {
-    return (
-        <div class="top_content">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-6 text-center">
-                    <h2>Let's talk about everything!
-                    </h2>
-                    <figure>
-                        <img src={ContactImage} alt="contact" />
-                    </figure>
-                </div>
-                <div class="col-md-6">
-                    <input type="text" class="form-control mb-3" placeholder="Your Name" />
-                    <input type="text" class="form-control mb-3" placeholder="Email" />
-                    <input type="text" class="form-control mb-3" placeholder="Subject" />
-                    <textarea name="" id="" cols="30" rows="6" placeholder="Your Message" class="form-control mb-3"></textarea>
-                    <div class="cf"></div>
-                    <input type="button" value="Send" class="btn btn_theme" />
-                </div>
-            </div>
+function ContactContent(props) {
+  const {
+    nameHandler,
+    emailHandler,
+    subjectHandler,
+    messageHandler,
+    formSubmitHandler,
+    output,
+    spinnerState,
+  } = props;
+
+  const form = spinnerState ? <SmallSpinner /> : (
+    <>
+      <input
+        type="text"
+        className="form-control mb-3"
+        placeholder="Your Name"
+        onChange={(e) => nameHandler(e.target.value)}
+      />
+      <input
+        type="email"
+        className="form-control mb-3"
+        placeholder="Email"
+        onChange={(e) => emailHandler(e.target.value)}
+      />
+      <input
+        type="text"
+        className="form-control mb-3"
+        placeholder="Subject"
+        onChange={(e) => subjectHandler(e.target.value)}
+      />
+      <textarea
+        name=""
+        id=""
+        cols="30"
+        rows="6"
+        placeholder="Your Message"
+        className="form-control mb-3"
+        onChange={(e) => messageHandler(e.target.value)}
+      ></textarea>
+      <div className="cf"></div>
+      <input
+        type="button"
+        value="Send"
+        className="btn btn_theme"
+        onClick={() => formSubmitHandler()}
+      />
+      {output ? <div className="message_display">{output}</div> : null}
+    </>
+  );
+
+  return (
+    <div className="top_content">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6 text-center">
+            <h2>Let's talk about everything!</h2>
+            <figure>
+              <img src={ContactImage} alt="contact" />
+            </figure>
+          </div>
+          <div className="col-md-6">
+            {form}
+          </div>
         </div>
+      </div>
     </div>
-    )
+  );
 }
 
-export default ContactContent
+export default ContactContent;
